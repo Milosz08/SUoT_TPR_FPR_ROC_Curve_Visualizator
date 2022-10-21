2bacf5795fd9ca662d68901b4c8b1af01bef2e79
Milosz08 <gilgamilosz451@gmail.com>
2022-10-21 21:46:04 +0200

    Add project files.

---
+++ TprFprCalculatorAndRocCurveVisualizator/ObAndTarget.cs
@@ -0,0 +1,14 @@
+﻿namespace RocCurveAICalculator
+{
+    public class ObAndTarget
+    {
+        public double Ob { get; set; }
+        public string Target { get; set; }
+    }
+
+    public class ConfusionMatrixData
+    {
+        public double[,] ConfusionMatrix { get; set; }
+        public string ObEstimate { get; set; }
+    }
+}
